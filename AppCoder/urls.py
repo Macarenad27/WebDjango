a160bf3e314e117c6602eee528e4b0cd855782e2
Maca Diaz <diaz.macarenaa@gmail.com>
2022-09-20 12:45:00 -0300

    Formulario API

--- AppCoder/urls.py
+++ AppCoder/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 from AppCoder.views import *
 
+
 urlpatterns = [
     path('', index),
     path('cursos2/', cursos2),
     path('estudiantes/', estudiantes),
     path('profesores/', profesores),
     path('entregables/', entregables),
-    path('home/', home)
+    path('home/', home),
+    path('api_estudiantes/', api_estudiantes)
 ]
